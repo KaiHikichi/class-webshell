7f608497d111dd60a02c4a483a0cf05a68d487b7
KaiHikichi <kai.hikichi@gmail.com>
2026-05-23 16:47:33 -0700

    added admin login

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const STUDENTS_FILE = path.join(__dirname, 'students.json');
+const ADMINS_FILE = path.join(__dirname, 'admins.json');
 tokens = new Map();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,82 +22,97 @@ app.use(express.json());
 //routes
 
 app.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
-  const students = JSON.parse(fs.readFileSync(STUDENTS_FILE, 'utf8'));
-  const student = students.find(s => s.username === username && s.password === password);
-  if (!student) return res.status(401).json({ error: 'Invalid username or password' });
+	const { username, password } = req.body;
+	const students = JSON.parse(fs.readFileSync(STUDENTS_FILE, 'utf8'));
+	const admins = JSON.parse(fs.readFileSync(ADMINS_FILE, 'utf8'));
 
-  const token = crypto.randomBytes(32).toString('hex')
-  tokens.set(token, { username: 'alice', role: 'student', expires: Date.now() + 8 * 60 * 60 * 1000 });
-  res.json({ token, username });
+	//check if admin
+	let role = 'admin';
+	let user = admins.find(s => s.username === username && s.password === password);
+	if (!user){
+		//check if student
+		role = 'student';
+		user = students.find(s => s.username === username && s.password === password);
+		if(!user){
+			//reject user
+			role = null;
+			return res.status(401).json({ error: 'Invalid username or password' });
+		}
+	}
+
+	const token = crypto.randomBytes(32).toString('hex')
+	tokens.set(token, { username: username, role: role, expires: Date.now() + 8 * 60 * 60 * 1000 });
+	res.json({ token, username, role });
 });
 
 function validateToken(token) {
-  const session = tokens.get(token)  
-  
-  if (!session) return null       
-  
-  if (Date.now() > session.expires) {
-    tokens.delete(token)          
-    return null
-  }
-  
-  return session                    
+	const session = tokens.get(token)  
+	
+	if (!session){
+		return null       
+	}
+	
+	if (Date.now() > session.expires) {
+		tokens.delete(token)          
+		return null
+	}
+	
+	return session                    
 }
 
 wss.on('connection', (ws, req) => {
-  const url = new URL(req.url, 'http://x');
-  const username = url.searchParams.get('username');
-  const token = url.searchParams.get('token');
+	const url = new URL(req.url, 'http://x');
+	const username = url.searchParams.get('username');
+	const token = url.searchParams.get('token');
 
-  // check token is valid
-  const session = validateToken(token)       
-  if (!session) {
-    ws.close() 
-    return
-  }
+	// check token is valid
+	const session = validateToken(token)       
+	if (!session) {
+		ws.close() 
+		return
+	}
 
-  const shell = pty.spawn('docker', [
-    'run', '--rm', '-it',
-    '--name', `student-${username}`,
-    '--memory', '128m',
-    '--cpus', '0.5',
-    '--pids-limit', '50',
-    '--security-opt=no-new-privileges',
-    '--cap-drop', 'ALL',
-    '--mount', `type=volume,source=student-${username},target=/home/student`,
-    'classroom-student'
-  ], {
-    name: 'xterm-color',
-    cols: 80, rows: 24,
-    env: { ...process.env, TERM: 'xterm-256color' }
-  });
+	const shell = pty.spawn('docker', [
+		'run', '--rm', '-it',
+		'--name', `student-${username}`,
+		'--memory', '128m',
+		'--cpus', '0.5',
+		'--pids-limit', '50',
+		'--security-opt=no-new-privileges',
+		'--cap-drop', 'ALL',
+		'--mount', `type=volume,source=student-${username},target=/home/student`,
+		'classroom-student'
+	], {
+		name: 'xterm-color',
+		cols: 80, rows: 24,
+		env: { ...process.env, TERM: 'xterm-256color' }
+	});
 
-  shell.onData((data) => {
-    if (ws.readyState === WebSocket.OPEN)
-      ws.send(JSON.stringify({ type: 'output', data }));
-  });
+	shell.onData((data) => {
+		if (ws.readyState === WebSocket.OPEN)
+			ws.send(JSON.stringify({ type: 'output', data }));
+	});
 
-  shell.onExit(({ exitCode }) => {
-    if (ws.readyState === WebSocket.OPEN) {
-      ws.send(JSON.stringify({ type: 'exit', exitCode }));
-      ws.close();
-    }
-  });
+	shell.onExit(({ exitCode }) => {
+		if (ws.readyState === WebSocket.OPEN) {
+			ws.send(JSON.stringify({ type: 'exit', exitCode }));
+			ws.close();
+		}
+	});
 
-  ws.on('message', (message) => {
-    try {
-      const msg = JSON.parse(message);
-      if (msg.type === 'input')  shell.write(msg.data);
-      if (msg.type === 'resize') shell.resize(msg.cols, msg.rows);
-    } catch (e) { console.error('Bad message:', e); }
-  });
+	ws.on('message', (message) => {
+		try {
+			const msg = JSON.parse(message);
+			if (msg.type === 'input')  shell.write(msg.data);
+			if (msg.type === 'resize') shell.resize(msg.cols, msg.rows);
+		} catch (e) { console.error('Bad message:', e); }
+	});
 
-  ws.on('close', () => { shell.kill(); });
+	ws.on('close', () => { shell.kill(); });
 });
 
 
 const PORT = process.env.PORT || 8888;
 server.listen(PORT, () => {
-  console.log(`Terminal server running at http://localhost:${PORT}`);
+	console.log(`Terminal server running at http://localhost:${PORT}`);
 });
